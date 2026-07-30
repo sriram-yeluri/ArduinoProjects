d18434b7a3726eb7c6bb82f12aad012189f0fdae
Sriram Yeluri <sriram.yeluri@outlook.com>
2026-07-30 11:55:37 +0530

    Add files via upload

---
+++ esp32/NTP-Clock/WiFiConfig.h
@@ -0,0 +1,9 @@
+// WiFiConfig.h
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+// Default WiFi credentials — change these or set via web config
+#define DEFAULT_SSID "yeluris"
+#define DEFAULT_PASSWORD "SmartKids@2024"
+
+#endif // WIFI_CONFIG_H
